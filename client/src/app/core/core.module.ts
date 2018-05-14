e7f2d5fcb7ff6d2f6eca823e317528121d0f7048
Unknown <personal@rjlopezmad.com>
2018-05-14 19:43:38 +0200

    Refactor | Split navbar from Layout

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  declarations: [LayoutComponent, FooterComponent]
+  declarations: [LayoutComponent, FooterComponent, NavbarComponent]
 })
 export class CoreModule { }
--- client/src/app/core/layout/layout.component.ts
+++ client/src/app/core/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
