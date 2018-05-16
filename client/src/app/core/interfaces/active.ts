7157392801d032e9c23dc7991b475403b3b20ce9
Unknown <personal@rjlopezmad.com>
2018-05-16 03:25:12 +0200

    Add Active and ClientRequest interfaces

---
+++ client/src/app/core/interfaces/active.ts
@@ -0,0 +1,7 @@
+export interface Active {
+    id: number;
+    name: string;
+    address: string;
+    description: string;
+    available: boolean;
+}
---
+++ client/src/app/core/interfaces/client-request.ts
@@ -0,0 +1,9 @@
+export interface ClientRequest {
+    id: number;
+    active_id: number,
+    name: string;
+    surname: string;
+    number_phone: string;
+    email: string;
+    description: string;
+}
